de0f0e0d4a489de4fe5a242d4cfc2bfdd5f8ddb6
Stephan Joseph <stephanjoseph2007@gmail.com>
2016-12-07 16:06:58 -0500

    update react one list

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-
+  constructor () {
+    super()
+    this.state = {
+      items: ['apple', 'banana', 'potato']
+    }
+  }
   render () {
+    const items = this.state.items.map((item) => {
+      return <li> {item}</li>
+    })
     return <div>
-      <h1>Hello, World!</h1>
+      <header>
+        <h1>One List!</h1>
+      </header>
+      <ul>
+        {items}
+      </ul>
+      <form>
+        <input type='text' name='todo' placeholder='Wat?' />
+      </form>
     </div>
   }
 }
